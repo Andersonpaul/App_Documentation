f162d78102b079280e292abb1d5ba65a40fb71f2
Andersonpaul <andersonudokoro@yahoo.com>
2015-03-24 13:09:25 +0100

    Field Attributes

---
+++ CIPG_Document/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CIPG_Documentation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver;
+            IWebElement element;
+            driver = new ChromeDriver();
+            driver.Url = "http://yahoomail.com/";
+            element = driver.FindElement(By.Name("username"));
+            string s = element.GetAttribute("maxlength");
+            Console.WriteLine(s);
+        }
+    }
+}
